3cd29c52269632f28e17b257330de71a8ae60429
elyes5e <elyes1929@gmail.com>
2024-10-28 01:40:29 -0400

    intermediateStep

--- pnpdm/samplers/__init__.py
+++ pnpdm/samplers/__init__.py
@@ -1,9 +1,12 @@
 from .pnp_edm.pnp_edm import PnPEDM, PnPEDMBatch
 from .pnp_edm.pnp_edm_bh import PnPEDMBH, PnPEDMBHBatch
+from .pnp_edm.pnp_edm_int import PnPEDMINT, PnPEDMBatchINT
 
 def get_sampler(config, model, operator, noiser, device):
     if config.name == 'pnp_edm':
         return PnPEDM(config, model, operator, noiser, device)
+    elif config.name == 'pnp_edm_int':
+        return PnPEDMINT(config, model, operator, noiser, device)
     elif config.name == 'pnp_edm_batch':
         return PnPEDMBatch(config, model, operator, noiser, device)
     elif config.name == 'pnp_edm_bh':

d126efa5404c776fe245294c1f257a9fafeb30bf
Brady Bhalla <ugcs fake email>
2025-05-18 18:16:54 -0700

    set sd wrapper correctly

--- scripts/dcdp_experiments.sh
+++ scripts/dcdp_experiments.sh
@@ -1,20 +1,20 @@
 python posterior_sample.py \
     +data=images_with_prompts \
     +task=super_resolution_svd \
-    +model=dcdp_sd_wrapper \
+    +model=daps_sd_wrapper \
     +sampler=dcdp \
     add_exp_name=dcdp_superres
 
 python posterior_sample.py \
     +data=images_with_prompts \
     +task=gaussian_deblur_circ \
-    +model=dcdp_sd_wrapper \
+    +model=daps_sd_wrapper \
     +sampler=dcdp \
     add_exp_name=dcdp_deblur
 
 python posterior_sample.py \
     +data=images_with_prompts \
     +task=box_inpainting \
-    +model=dcdp_sd_wrapper \
+    +model=daps_sd_wrapper \
     +sampler=dcdp \
     add_exp_name=dcdp_box
